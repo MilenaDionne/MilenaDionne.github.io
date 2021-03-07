183c8f8ff735920b203f591cb5c10bbf7169936a
Jean-Luc Blais-Amyot <35350889+amyotjl@users.noreply.github.com>
2021-03-07 16:42:46 -0500

    not finished

--- lab5_seg3125/scripts/main.js
+++ lab5_seg3125/scripts/main.js
@@ -3,3 +3,19 @@
 $(function() {
     $('[data-toggle="tooltip"]').tooltip()
 })
+
+jQuery.validator = function(regex, str) {
+    var reg = new RegExp(regex)
+    return reg.test(str)
+}
+
+$('#cc-number').keydown(function() {
+    var number = $('#cc-number').val().replaceAll(' ', '').replaceAll('-', '');
+
+    var isValid = jQuery.validator('[0-9]{15}', number);
+    if (isValid) {
+
+    } else {
+        $('#cc-number').addClass(['border', 'border-5', 'border-danger'])
+    }
+})
