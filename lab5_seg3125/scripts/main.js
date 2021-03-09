3404aae443b9be0984a1f79dfbeef0a2af5ff804
Jean-Luc Blais-Amyot <35350889+amyotjl@users.noreply.github.com>
2021-03-09 11:23:20 -0500

    cc validation

--- lab5_seg3125/scripts/main.js
+++ lab5_seg3125/scripts/main.js
@@ -8,50 +8,94 @@ jQuery.validator = function(regex, str) {
     var reg = new RegExp(regex)
     return reg.test(str)
 }
+var cc_validation = {
+    '#cc-number': false,
+    '#cc-cvv': false,
+    '#cc-name': false,
+    '#cc-expiration': false
+}
 
 //Validates the input with a regex
 //Adjust the tooltip with the validation
-jQuery.cc_number_validator = function(id, regex, invalid_toottip) {
+jQuery.cc_validator = function(id, regex, invalid_message) {
     var number = $(id).val().replaceAll(' ', '').replaceAll('-', '');
-    var isValid = jQuery.validator(regex, number) && number.length == $(id).attr('maxLength');
-
-    if (isValid) {
+    var maxLength = $(id).attr('maxLength') ? $(id).attr('maxLength') : number.length
+    var isValid = jQuery.validator(regex, number) && number.length == maxLength;
+    var expirationMonthValid = true
+    if (id == "#cc-expiration") {
+        var month = parseInt($(id).val().slice(0, 2));
+        if (month > 12 || month < 1) {
+            expirationMonthValid = false
+        }
+    }
+    //If the input is valid, we make the border green
+    if (isValid && expirationMonthValid) {
         $(id).removeClass(['border', 'border-3', 'border-danger', ])
-        $(id).addClass(['border', 'border-3', 'border-success', 'ui-icon', 'ui-icon-circle-check'])
-        $(id).attr('data-original-title', 'This is the correct format');
-
+        $(id).addClass(['border', 'border-3', 'border-success'])
+        $(id + '-small-red').remove();
+        if ($(id + '-small-green').length == 0) {
+            $(id).after($('<small class="text-success" id="' + id.replace('#', '') + '-small-green"></small>').text('This is the correct format'))
+        }
 
     } else {
-        $(id).removeClass(['border', 'border-3', 'border-success', 'ui-icon', 'ui-icon-circle-check'])
-        $(id).addClass(['border', 'border-3', 'border-danger'])
-        $(id).attr({
-            'data-toggle': 'tooltip',
-            'data-placement': 'bottom',
-            'data-original-title': invalid_toottip
-        }).tooltip();
+        //If the input is invalid, we make the border red
+        $(id).removeClass(['border', 'border-3', 'border-success'])
+        $(id).addClass(['border', 'border-3', 'border-danger']);
+        $(id + '-small-green').remove();
+        if ($(id + '-small-red').length == 0) {
+            $(id).after($('<small class="text-danger" id="' + id.replace('#', '') + '-small-red"></small>').text(invalid_message))
+        }
     }
+    cc_validation[id] = isValid
+    return expirationMonthValid
 }
 
 jQuery.only_numbers = function(id) {
-    var text_replace = $(id).val().replace(/[^0-9.]/g, '').replace(/(\..*?)\..*/g, '$1');
+    var text_replace = $(id).val().replace(/[^0-9., /]/g, '').replace(/(\..*?)\..*/g, '$1');
     $(id).val(text_replace)
 }
 
 
-jQuery.only_letters = function(id) {
-    var text_replace = $(id).val().replace(/[0-9]/g, '')
-    console.log(text_replace)
+jQuery.cc_name_validator = function(id) {
+    var text_replace = $(id).val().replace(/[^A-Za-z, ,--,']/g, '')
     $(id).val(text_replace)
 }
 
 //Name
-$('#cc-name').on('input', function() { jQuery.only_letters('#cc-name') });
+$('#cc-name').on('input', function() { jQuery.cc_name_validator('#cc-name') });
+$('#cc-name').on('input', function() { jQuery.cc_validator('#cc-name', "[a]|[^a]", 'Only letters, spaces, dashes (-) and apostrophes (\')') });
 
 
 //Number
 $('#cc-number').on('input', function() { jQuery.only_numbers('#cc-number') })
-$('#cc-number').on('input', function() { jQuery.cc_number_validator('#cc-number', '[0-9]{15}', 'Requires 16 digits, no spaces') });
+$('#cc-number').on('input', function() { jQuery.cc_validator('#cc-number', '[0-9]{15}', 'Requires 16 digits, no spaces') });
 
 //CVV
-$('#cc-cvv').on('input', function() { jQuery.cc_number_validator('#cc-cvv', '[0-9]{2}', 'Requires 3 digits, no space. This number is located on the fron of your card') });
+$('#cc-cvv').on('input', function() { jQuery.cc_validator('#cc-cvv', '[0-9]{2}', 'Requires 3 digits, no spaces') });
 $('#cc-cvv').on('input', function() { jQuery.only_numbers('#cc-cvv') });
+
+//Expiration date
+$('#cc-expiration').on('input', function() { jQuery.only_numbers('#cc-expiration') });
+$('#cc-expiration').on('input', function(event) {
+    if ($(this).val().length == 2 && event.originalEvent.data != null) {
+        $(this).val($(this).val().slice(0, 2) + '/' + $(this).val().slice(2))
+    }
+    var ex = jQuery.cc_validator('#cc-expiration', '[0-9]{1}/[0-9]{1}', 'Requires 4 digits, no spaces. MM/YY');
+    console.log(ex)
+    if (!ex) {
+        $('#cc-expiration').removeClass(['border', 'border-3', 'border-success'])
+        $('#cc-expiration').addClass(['border', 'border-3', 'border-danger']);
+        $('#cc-expiration-small-green').remove();
+        $('#cc-expiration-small-red').remove();
+        if ($('#cc-expiration-small-red').length == 0) {
+            $('#cc-expiration').after($('<small class="text-danger" id="' + '#cc-expiration'.replace('#', '') + '-small-red"></small>').text('Month must be between 01 and 12'))
+        }
+    }
+});
+
+$('#cc-form').on('input', function() {
+    var c = Object.values(cc_validation).filter(bool => bool == false)
+    console.log(c.length)
+    var enableButton = Object.values(cc_validation).filter(bool => bool == false).length != 0
+    $('#cc-completed').prop('disabled', enableButton);
+});
