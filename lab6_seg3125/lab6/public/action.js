d324945efca8fd8033ee82522bc7fe5b3888cda3
Jean-Luc Blais-Amyot <35350889+amyotjl@users.noreply.github.com>
2021-03-13 17:04:08 -0500

    minor fixes for analysis and server side

--- lab6_seg3125/lab6/public/action.js
+++ lab6_seg3125/lab6/public/action.js
@@ -8,7 +8,7 @@ $(document).ready(function() {
 
         $.ajax({
             type: 'POST',
-            url: '/UOSurvey',
+            url: '/index',
             data: $(this).serializeArray(),
             success: function(data) {
                 console.log("Sent with ", data)
