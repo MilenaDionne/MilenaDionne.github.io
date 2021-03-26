a55f3a7fc6a20d5b5acff9c441e08f97f06fa236
Jean-Luc Blais-Amyot <35350889+amyotjl@users.noreply.github.com>
2021-03-26 14:15:54 -0400

    item example/template

--- lab8_9_seg3125/seg3125/src/App.js
+++ lab8_9_seg3125/seg3125/src/App.js
@@ -1,9 +1,39 @@
-function App() {
-  return (
-    <div className="App">
+import React, { Component } from 'react';
+class App extends Component {
+  size = {
+    XS: 'Extra Small',
+    S: 'Small',
+    M: 'Medium',
+    L: 'Large',
+    XL: 'Extra Large',
+    XXL: 'Extra Extra Large'
+  }
 
-    </div>
-  );
+  state = {
+    idCount: 2,
+
+    items: [{
+      id: 1,
+      name: 'Blue T-Shirt',
+      imgSrc: '../images/Blue_T-Shirt_M_Blue_None_30_JL.jpg',
+      cost: 30,
+      size: this.size.M,
+      mainColor: 'Blue',
+      secondarColor: null,
+      description: 'Perfect T-Shirt for biking',
+      owner: {
+        name: 'JL',
+        email: 'fakeemail@gmail.com',
+        phone: '8191234568'
+      }
+    }]
+  }
+  render() {
+    return (
+      <div className="App" >
+      </div>
+    );
+  }
 }
 
 export default App;
