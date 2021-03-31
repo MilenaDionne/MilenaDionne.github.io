ea68d174713a6f7e39df4833de2cd0f33bec16e6
Yousuf Ahmed <38750706+ym-ahmed@users.noreply.github.com>
2021-03-30 21:14:30 -0400

    started work on search bar component

---
+++ lab8_9_seg3125/seg3125/src/SearchBar.js
@@ -0,0 +1,19 @@
+// JavaScript source code
+import React, { Component } from 'react'
+import { render } from 'react-dom';
+import ReactTooltip from "react-tooltip";
+
+class SearchBar extends Component {
+    render(){
+    return (){
+        <center>
+            <form>
+                <div style="margin-top:100px" >
+                    <input type="text" placeholder="Search..">
+                    <button type="submit">Submit</button>
+                 </div>
+            </form>
+        </center>
+    }
+}
+}
