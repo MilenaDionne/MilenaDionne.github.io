05a6a5452f2c510ae416cf4a8f242b5da5776d20
Jean-Luc Blais-Amyot <35350889+amyotjl@users.noreply.github.com>
2021-03-31 17:49:21 -0400

    added empty filters

--- lab8_9_seg3125/seg3125/src/Filter.js
+++ lab8_9_seg3125/seg3125/src/Filter.js
@@ -1,22 +1,20 @@
 // JavaScript source code
 import React, { Component } from 'react';
-import { render } from 'react-dom';
-import ReactTooltip from "react-tooltip";
-
 
 class Filter extends Component {
 
    render() {
       const { name, value, filters } = this.props;
+      const valueList = filters.map(value => {
+         return (
+            <option>{value}</option>
+         )
+      })
       return (
          <form>
             <label for="filters">Filter by {name}&nbsp;</label>
             <select name="filters" id="filters">
-               <option>{filters[0]}</option>
-               <option>{filters[1]}</option>
-               <option>{filters[2]}</option>
-               <option>{filters[3]}</option>
-               <option>{filters[4]}</option>
+               {valueList}
             </select>
          </form>
       );
--- lab8_9_seg3125/seg3125/src/FilterContainer.js
+++ lab8_9_seg3125/seg3125/src/FilterContainer.js
@@ -9,13 +9,13 @@ class FilterContainer extends Component {
     render() {
         return (
             <div>
-            <Filter name="size" value="none" filters={["extra small", "small", "meduim", "large", "extra large"]} />
-            <br></br>
-            <Filter name="price" value="none" filters={["0-9", "10-19", "20-29", "30-39", "40+"]} />
-            <br></br>
-            <Filter name="type" value="none" filters={["headwear", "shirt", "pants", "socks", "shoes"]}/>
+                <Filter name="size" value="none" filters={["", "extra small", "small", "meduim", "large", "extra large"]} />
+                <br></br>
+                <Filter name="price" value="none" filters={["", "0-9", "10-19", "20-29", "30-39", "40+"]} />
+                <br></br>
+                <Filter name="type" value="none" filters={["", "headwear", "shirt", "pants", "socks", "shoes"]} />
             </div>
-            );
+        );
     }
 
 
