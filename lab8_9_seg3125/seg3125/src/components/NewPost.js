e5eebee5394874bbf70f6494d01ba7e86bdc42cf
milena dionne <milena.dionne@olympus.com>
2021-12-16 20:33:04 -0500

    Revert "Revert "Merge branch 'master' of https://github.com/MilenaDionne/MilenaDionne.github.io""
    This reverts commit 173c757db8d1ae82a956710f0e6f361770205de8.

--- lab8_9_seg3125/seg3125/src/components/NewPost.js
+++ lab8_9_seg3125/seg3125/src/components/NewPost.js
@@ -8,96 +8,98 @@ import './NewPost.css'
 
 class NewPost extends Component {
 
-    flag = false; 
+    flag = false;
     state = {
         imgSrc: '../../images/addImage.png'
     }
 
-       
+
     handleChange = (e) => {
         console.log('target id: ', e.target.id)
-        if (e.target.id === 'imgSrc'){
+        if (e.target.id === 'imgSrc') {
             this.setState({
                 [e.target.id]: URL.createObjectURL(e.target.files[0])
             })
 
-        } else if(e.target.id === 'agreement'){
+        } else if (e.target.id === 'agreement') {
 
-        }else {
+        } else {
             this.setState({
                 [e.target.id]: e.target.value
             })
         }
 
         var bt = document.getElementById('submit');
-        var type = document.getElementById('type'); 
-        var size = document.getElementById('size'); 
-        var name = document.getElementById('name'); 
-        var cost = document.getElementById('cost'); 
-        var owneremail = document.getElementById('owneremail'); 
-        var ownerphone = document.getElementById('ownerphone'); 
-        var mainColor = document.getElementById('mainColor'); 
-        var agreement = document.getElementById('agreement'); 
+        var type = document.getElementById('type');
+        var size = document.getElementById('size');
+        var name = document.getElementById('name');
+        var cost = document.getElementById('cost');
+        var owneremail = document.getElementById('owneremail');
+        var ownerphone = document.getElementById('ownerphone');
+        var mainColor = document.getElementById('mainColor');
+        var agreement = document.getElementById('agreement');
         if (size.value !== '' && type.value !== '' && agreement.checked === true && name.value !== '' && mainColor.value !== '' && cost.value !== '' && (owneremail.value !== '' || ownerphone !== '')) {
             bt.disabled = false;   // Enable the button.
-            
+
         }
         else {
             bt.disabled = true;   // Disable the button.
         }
-        
+
     }
 
     handleSubmit = (e) => {
         e.preventDefault();
         this.props.addNewPost(this.state);
-        
+
         this.closeModal();
 
     }
 
-   openModal() {
+    openModal() {
         document.getElementById("backdrop").style.display = "block"
         document.getElementById("newPostModal").style.display = "block"
         document.getElementById("newPostModal").className += "show"
     }
 
-    closeModal() {
+    closeModal = () => {
         document.getElementById("backdrop").style.display = "none"
         document.getElementById("newPostModal").style.display = "none"
         document.getElementById("newPostModal").className += document.getElementById("newPostModal").className.replace("show", "")
+        document.getElementById("newPostModalForm").reset();
+        this.setState({ imgSrc: '../../images/addImage.png' })
     }
 
 
 
     render() {
-        const {language, filters, sizes, colors} = this.props;
+        const { language, filters, sizes, colors } = this.props;
 
-        
+
 
         const valueColors = colors.map((value, key) => {
             return (
-               <option key={key}>{l(language, value)}</option>
-            )
-         })
-         
-         const clothsType = filters.values.map((value, key) => {
-            return (
-               <option key={key}>{l(language, value)}</option>
-            )
-         })
-
-         const clothsSize = sizes.map((value, key) => {
-             return (
                 <option key={key}>{l(language, value)}</option>
-             )
-         })
+            )
+        })
+
+        const clothsType = filters.values.map((value, key) => {
+            return (
+                <option key={key}>{l(language, value)}</option>
+            )
+        })
+
+        const clothsSize = sizes.map((value, key) => {
+            return (
+                <option key={key}>{l(language, value)}</option>
+            )
+        })
 
         return (
             <Col >
                 <button type="button" className="btn w-100 bgNewPost" onClick={this.openModal}>
                     {l(language, 'NewPostTitle')}
-            </button>
+                </button>
                 <div className="modal fade" id="newPostModal" tabIndex="-1" role="dialog" aria-labelledby="newPostModalLabel" aria-modal="true">
                     <div className="modal-dialog modal-xl" role="document">
                         <div className="modal-content">
@@ -107,7 +109,7 @@ class NewPost extends Component {
                                     <span aria-hidden="true">&times;</span>
                                 </button>
                             </div>
-                            <Form onSubmit={this.handleSubmit}>
+                            <Form onSubmit={this.handleSubmit} id="newPostModalForm">
                                 <div className="modal-body">
                                     <Form.Row>
                                         <div className="col-5">
@@ -116,7 +118,7 @@ class NewPost extends Component {
                                                     <input type="file" id="imgSrc" accept=".jpg, .jpeg, .png, .jfif" onChange={this.handleChange} />
                                                 </label>
                                             </Row>
-                                            <div className="row-cols-2">
+                                            <div className="row-cols-2 text-center ">
                                                 <Image src={this.state.imgSrc} className="img-fluid" alt="Responsive image"></Image>
                                             </div>
                                         </div>
@@ -124,10 +126,10 @@ class NewPost extends Component {
                                         <div className="col-6">
                                             <Form.Group as={Row} className="was-validated">
                                                 <Form.Label column sm="2">
-                                                {l(language, 'Name')}
+                                                    {l(language, 'Name')}
                                                 </Form.Label>
                                                 <Col sm="10">
-                                                    <Form.Control type="name" id="name" className ="is-invalid" placeholder={l(language, 'Name')} onChange={this.handleChange} aria-describedby="validationServerUsernameFeedback" required/>
+                                                    <Form.Control type="name" id="name" className="is-invalid" placeholder={l(language, 'Name')} onChange={this.handleChange} aria-describedby="validationServerUsernameFeedback" required />
                                                 </Col>
                                             </Form.Group>
                                             <Form.Group as={Row} className="was-validated">
@@ -143,10 +145,10 @@ class NewPost extends Component {
                                             </Form.Group>
                                             <Form.Group as={Row} className="was-validated">
                                                 <Form.Label column sm="2">
-                                                {l(language, 'ItemPrice')}
+                                                    {l(language, 'ItemPrice')}
                                                 </Form.Label>
                                                 <Col sm="10">
-                                                    <Form.Control type="number" step="0.01" className ="is-invalid" id="cost" placeholder={l(language, 'ItemPrice')} onChange={this.handleChange} required/>
+                                                    <Form.Control type="number" step="0.01" className="is-invalid" id="cost" placeholder={l(language, 'ItemPrice')} onChange={this.handleChange} required />
                                                 </Col>
                                             </Form.Group>
                                             <Form.Group as={Row} className="was-validated">
@@ -162,7 +164,7 @@ class NewPost extends Component {
                                             </Form.Group>
                                             <Form.Group>
                                                 <Form.Label>Description</Form.Label>
-                                                <Form.Control as="textarea" id="description" rows={3} onChange={this.handleChange}/>
+                                                <Form.Control as="textarea" id="description" rows={3} onChange={this.handleChange} />
                                             </Form.Group>
                                             <Form.Group>
                                                 <Row>
@@ -181,27 +183,27 @@ class NewPost extends Component {
                                                 </Row>
                                             </Form.Group>
                                         </div>
-                                    </Form.Row> 
+                                    </Form.Row>
                                     <div>
                                         <h4>{l(language, 'NewContactInfo')}</h4>
                                     </div>
                                     <Row>
                                         <Col>
-                                            <Form.Control placeholder={l(language, 'NewContactName')} id="ownername" onChange={this.handleChange}/>
-                                            
+                                            <Form.Control placeholder={l(language, 'NewContactName')} id="ownername" onChange={this.handleChange} />
+
                                         </Col>
                                         <Col className="was-validated">
-                                            <Form.Control type="email" placeholder={l(language, 'NewContactEmail')} id="owneremail" className ="is-invalid" onChange={this.handleChange} required/>
+                                            <Form.Control type="email" placeholder={l(language, 'NewContactEmail')} id="owneremail" className="is-invalid" onChange={this.handleChange} required />
                                         </Col>
                                     </Row>
                                     <br></br>
                                     <Form.Row>
                                         <Col>
-                                            <Form.Control pattern="[0-9]{10}" placeholder={l(language, 'NewContactPhone')} title="Phone number should be 10 digits" id="ownerphone" onChange={this.handleChange}/>
+                                            <Form.Control pattern="[0-9]{10}" placeholder={l(language, 'NewContactPhone')} title="Phone number should be 10 digits" id="ownerphone" onChange={this.handleChange} />
                                         </Col>
                                         <Col className="was-validated">
                                             <Form.Group>
-                                                <Form.Check type="checkbox" id="agreement" label={l(language, 'NewAgreementConditions')} onChange={this.handleChange} required/>
+                                                <Form.Check type="checkbox" id="agreement" label={l(language, 'NewAgreementConditions')} onChange={this.handleChange} required />
                                             </Form.Group>
                                         </Col>
                                     </Form.Row>
@@ -214,7 +216,7 @@ class NewPost extends Component {
                         </div>
                     </div>
                 </div>
-                <div className="modal-backdrop fade show" id="backdrop" style={{display: 'none'}}></div>
+                <div className="modal-backdrop fade show" id="backdrop" style={{ display: 'none' }}></div>
             </Col>
 
         )
